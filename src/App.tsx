b17e87aa4a8db4a18c11ab934801fcd20613471c
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-04-05 23:01:31 -0300

    add: routes home and history

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { Globalstyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello, Wolrd!</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <Globalstyle />
     </ThemeProvider>
       
---
+++ src/pages/History.tsx
@@ -0,0 +1,9 @@
+function History(){
+  return(
+    <div id="history">
+      <h1>History </h1>
+    </div>
+  );
+}
+
+export { History };
