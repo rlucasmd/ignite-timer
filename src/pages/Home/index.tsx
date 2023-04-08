57019b9829c5ff99336afd349031bc92519f1fb2
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-04-07 22:34:39 -0300

    add: styled inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { 
+  CountdownContainer, 
+  FormContainer, 
+  HomeContainer, 
+  MinutesAmountInput, 
+  Separator, 
+  StartCountdownButton,
+  TaskInput, 
+} from "./styles";
 
 function Home(){
   return(
@@ -7,10 +15,29 @@ function Home(){
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" type="text" />
+          <TaskInput 
+            id="task" 
+            type="text" 
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount"/>
+          <MinutesAmountInput
+            placeholder="00" 
+            type="number" 
+            id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -23,10 +50,10 @@ function Home(){
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit">
           <Play size={24}/>
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -56,4 +56,76 @@ const Separator = styled.div`
   justify-content: center;
 `;
 
-export { HomeContainer, CountdownContainer, FormContainer, Separator };
+const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${props => props.theme["green-500"]};
+  color: ${props => props.theme["gray-100"]};
+
+  transition: background 0.3s;
+
+  &:disabled{
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover{
+    background: ${props => props.theme["green-700"]};
+  }
+`;
+
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${props => props.theme["gray-500"]};
+
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+
+  color: ${props => props.theme["gray-100"]};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${props => props.theme["green-500"]};
+  }
+
+  &::placeholder{
+
+  }
+`;
+
+const TaskInput = styled(BaseInput)`
+  flex: 1;
+  &::-webkit-calendar-picker-indicator{
+    display: none !important;
+  }
+`;
+
+const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
+
+
+export { 
+  HomeContainer, 
+  CountdownContainer, 
+  FormContainer, 
+  Separator, 
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput
+};
